65501c702e5e02769c336ca90fffe79c8000320a
AshwiniRatod. <ashuratod40@gmail.com>
2025-12-27 03:20:21 +0530

    fix port

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ const app = express();
 
 // ---------- CORS CONFIGURATION ----------
 app.use(cors({
-  origin: "https://your-frontend.vercel.app", // replace with your Vercel frontend URL
+  origin: ["http://localhost:3000", "https://task-manager-git-main-ashuratod40-gmailcoms-projects.vercel.app"], // replace with your Vercel frontend URL
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true, // if you are using cookies/auth
 }));
@@ -23,6 +23,8 @@ app.use(express.json());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/tasks", taskRoutes);
-
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.get("/", (req, res) => {
+  res.send("Backend is running 🚀");
+});

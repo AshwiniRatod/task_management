1c5a3d0ceb30a8938c068de4f5ecc8b2cd3e0caa
AshwiniRatod. <ashuratod40@gmail.com>
2025-12-27 08:56:45 +0530

    url updates

--- task-manager/src/api/axios.js
+++ task-manager/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://task-management-emc8.onrender.com/api",
+  baseURL: "https://task-management-emc8.onrender.com/api/tasks",
 });
 
 // ✅ Attach token automatically
